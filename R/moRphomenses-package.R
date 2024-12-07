60578438c63c69f1149c7567349e056e32077590
Dan Ehrlich <ehrli097@umn.edu>
2024-12-07 13:43:11 -0600

    revising dependencies to avoid errors

--- R/moRphomenses-package.R
+++ R/moRphomenses-package.R
@@ -7,17 +7,10 @@
 #'
 #' @description Analyze shapes/phenotypes of hormone data using Geometric Morphometric inspired methods.
 #'
-#'
+#' @import stats
 #' @import graphics
 #' @import grDevices
 #' @import utils
-#' @importFrom cluster pam
-#' @importFrom dendextend labels_colors
-#' @importFrom dendextend place_labels
-#' @importFrom dendextend cutree
-#' @importFrom geomorph geomorph.data.frame
-#' @importFrom geomorph procD.lm
-#' @importFrom geomorph shape.predictor
 #'
 #'
 #'

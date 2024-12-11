2e08b2b1b1c9793c53dd57cade6551fc79b5518c
Dan Ehrlich <ehrli097@umn.edu>
2024-12-11 09:54:22 -0600

    incrementing ahead of release

--- R/explore.R
+++ R/explore.R
@@ -203,18 +203,18 @@ mm_Diagnostics <- function(dat, max_PC_viz=10, max_PC_calc=NULL, hide_plots = FA
 #' @return If plot_figs=TRUE (Default), plot associated graphs and return a list containing:
 #' \itemize{
 #'   \item  ALN - an array containing aligned and scaled landmark data, the
-#'   output of \code{\link{mm_ArrayData}}
+#'     output of \code{\link{mm_ArrayData}}
 #'   \item PCA - PC scores, eigenvalues, and shape visualizations, the output of
-#'    \code{\link{mm_CalcShapespace}}
-#'   \itemize TREE - Dendrogram of PC scores, the output of
-#'    \code\link{mm_Diagnostics}}
+#'     \code{\link{mm_CalcShapespace}}
+#'   \item TREE - Dendrogram of PC scores, the output of
+#'     \code\link{mm_Diagnostics}}
 #'   \item k_grps - If `kgrps` is specified, a vector defining group membership
-#'   (as integer); the results of k-means clustering based on PC scores.
+#'     (as integer); the results of k-means clustering based on PC scores.
 #'   \item cth_grps - If `cth_grps` is specified, a vector defining group
-#'   membership (as integer); the results of clustering using
-#'   [dendextend::cutree] for a given height.
+#'     membership (as integer); the results of clustering using
+#'     [dendextend::cutree] for a given height.
 #'   \item ctk_grps - If `ctk_grps` is specified, a vector defining group
-#'   membership (as integer); the results of clustering using
+#'     membership (as integer); the results of clustering using
 #'   [dendextend::cutree] for a given number of clusters.
 #' }
 #' @export
